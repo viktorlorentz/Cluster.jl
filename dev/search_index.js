871f3d240bd918936d979a038f50a7127c18f1d4
Documenter.jl <documenter@juliadocs.github.io>
2024-05-29 21:18:41 +0000

    build based on 938fea7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Cluster","category":"page"},{"location":"#Cluster","page":"Home","title":"Cluster","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Cluster.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Cluster]","category":"page"}]
+}
