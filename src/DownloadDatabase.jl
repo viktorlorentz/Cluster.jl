9378df930d2001f3eb3fe0acabf0d853533c761d
Omadbek <35502107+Omadzze@users.noreply.github.com>
2024-06-04 18:15:00 +0200

    dataset download functionality

---
+++ src/DownloadDatabase.jl
@@ -0,0 +1,62 @@
+using LibGit2
+using GZip
+using DelimitedFiles
+using Clustering
+
+folder_name = "datasets"
+const datasetpath = joinpath(dirname(@__DIR__), folder_name)
+
+# Downloading dataset to test our algorithm from Github
+function download_data(url)
+    if !isdir(datasetpath)
+        mkdir(datasetpath)
+        LibGit2.clone(url, datasetpath)
+        print("Creating folder and downloading data...")
+    elseif isdir(datasetpath) && isempty(readdir(datasetpath))
+        print("Downloading data...")
+        LibGit2.clone(url, datasetpath)
+    else
+        print("Data exist")
+        data_preprocessing("wut", "x2")
+    end
+end
+
+# code taken from https://github.com/HolyLab/ClusteringBenchmarks.jl 
+# Preprocess our data after downloading. Note that data are in .data.gz
+# format so we need to extract it
+function data_preprocessing(battery, dataset)
+    basename = joinpath(datasetpath, battery, dataset)
+    datafile = basename * ".data.gz"
+    #print(datafile)
+    isfile(datafile) || error("no such file: $datafile")
+    data = Matrix(gzopen(datafile) do f
+        readdlm(f, Float64)'
+    end)
+    labels = Vector{Int}[]
+    i = 0
+    while true
+        labelfile = basename * ".labels$i.gz"
+        if !isfile(labelfile)
+            break
+        end
+        push!(labels, gzopen(labelfile) do f
+            vec(readdlm(f, Int))
+        end)
+        i += 1
+    end
+    isempty(labels) && error("no label files found for $basename")
+    return data, labels
+end
+
+# make calculation on data that was downloaded
+function data_calculation(data; noise_factor=1e-6)
+    mean = mean(data, dims=2)
+    centroid = data .- mean
+    std_dev = std(centroid, dims=2)
+    scaled = centroid ./ std_dev
+    noise = noise_factor * randn(size(data))
+    return scaled .+ noise
+end
+
+
+download_data("https://github.com/Omadzze/JlData.git")
