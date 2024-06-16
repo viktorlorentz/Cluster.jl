0467cb6361ca42c136248e77447654cf8b1e5e09
Viktor Lorentz <lorentz@campus.tu-berlin.de>
2024-06-17 00:00:22 +0200

    improve notebook

--- examples/notebook.jl
+++ examples/notebook.jl
@@ -18,11 +18,11 @@ end
 using Pkg
 
 # ╔═╡ 2dba0803-7a19-4fae-a6ea-de6f13423129
-begin 
-	Pkg.add("Plots")
-	Pkg.add("Random")
-	Pkg.add("PlutoUI")
-	Pkg.activate()
+begin
+    Pkg.add("Plots")
+    Pkg.add("Random")
+    Pkg.add("PlutoUI")
+    Pkg.activate()
 end
 
 # ╔═╡ d4687371-815f-4c67-9bdb-44a28c0d8122
@@ -47,57 +47,98 @@ function create_labeled_data(num_samples_per_class::Int, num_features::Int, num_
     data = []  # Initialize an empty list for data points
     labels = []  # Initialize an empty list for labels
     for i in 1:num_classes
-	
         center = randn((1,num_features)) * (spread * log(num_classes))  # Generate a random center for each class
-
-        class_data = [center + randn((1,num_features)) for _ in 1:num_samples_per_class]
-	# Create data points around the center
+        class_data = [center + randn((1,num_features)) for _ in 1:num_samples_per_class]  # Create data points around the center
         append!(data, class_data)  # Add the generated data points to the data list
         append!(labels, fill(i, num_samples_per_class))  # Add labels for each data point
     end
     return vcat(data...), labels  # Return the data and labels as a matrix and a vector
 end
 
-
 # ╔═╡ 0e11467a-ec17-43a0-bd29-4711dd5d8aee
-@bind num_samples_per_class Slider(5:200, default=20)
+@bind num_samples_per_class Slider(5:200, default=20, show_value=true)
 
 # ╔═╡ 146996c3-ea17-49e2-930e-40e1e04d7e49
-@bind num_features Slider(1:4 , default=2)
+@bind num_features Slider(1:4 , default=2, show_value=true)
 
 # ╔═╡ c060f1ea-0a86-4fa3-980a-57b08912e87e
-@bind num_classes Slider(1:50 , default=3)
+@bind num_classes Slider(1:50 , default=3, show_value=true)
 
 # ╔═╡ 853c6935-f73c-4076-98af-0896ca0cdae2
-@bind spread Slider(0:0.1:10 , default=10)
+@bind spread Slider(0:0.1:10 , default=4, show_value=true)
+
+# ╔═╡ fb517c86-f37a-44da-a5c0-9f97094215a5
+@bind clustering_algorithm Select(["kmeans", "kmeanspp", "bkmeans"])
 
 # ╔═╡ 726f9420-db77-442a-9fa3-09a1dad985c4
 begin
-	# Generate sample data
-	data, labels = create_labeled_data(num_samples_per_class, num_features, num_classes, spread = spread)
-
-	
-	# Plot the sample data
-	function plot_sample_data(data, labels, num_classes , title = "Sample Data")
-	    scatter()
-	    for i in 1:num_classes
-	        class_data = data[labels .== i,:]
-	        scatter!(class_data[:,1],class_data[:,2], label="Class $i")
-	    end
-	    xlabel!("Feature 1")
-	    ylabel!("Feature 2")
-	    title!(title)
-	end
-	
-	plot_sample_data(data, labels, num_classes)
+# Function to plot data
+function plot_sample_data!(ax, data, labels, num_classes, title = "Sample Data")
+    for i in 1:num_classes
+        class_data = data[labels .== i, :]
+        scatter!(ax, class_data[:, 1], class_data[:, 2], label="Class $i")
+    end
+    xlabel!(ax, "Feature 1")
+    ylabel!(ax, "Feature 2")
+    title!(ax, title)
 end
 
-# ╔═╡ 3197f17d-61c9-430c-9025-8e1de79e8f57
+# Function to create subplots
+function create_subplots(data, labels, predicted_labels, num_classes)
+    fig = plot(layout = (2, 1), size=(800, 1100))
+    plot_sample_data!(fig[1], data, labels, num_classes, "Sample Data")
+    plot_sample_data!(fig[2], data, predicted_labels, num_classes, "Prediction")
+    
+    # Mark wrong predictions with an "x"
+    wrong_predictions = labels .!= predicted_labels
+    scatter!(fig[2], data[wrong_predictions, 1], data[wrong_predictions, 2], marker=:x, color=:black, label="Wrong Prediction")
+    
+    return fig
+end
+
+# Function to match cluster labels
+function match_labels(true_labels, predicted_labels, num_classes)
+    contingency_matrix = zeros(Int, num_classes, num_classes)
+    for i in 1:num_classes
+        for j in 1:num_classes
+            contingency_matrix[i, j] = sum((true_labels .== i) .& (predicted_labels .== j))
+        end
+    end
+    
+    label_mapping = Dict{Int, Int}()
+    for i in 1:num_classes
+        max_overlap = argmax(contingency_matrix[:, i])
+        label_mapping[i] = max_overlap
+    end
+    
+    matched_labels = [label_mapping[label] for label in predicted_labels]
+    return matched_labels
+end
+
+# Main block
 begin
-	kmeans = Cluster.KMeans(k=num_classes, mode="kmeanspp")
-	Cluster.fit!(kmeans, data)
-	
-	plot_sample_data(data, kmeans.labels, num_classes, "Prediction")
+    # Generate sample data
+    data, labels = create_labeled_data(num_samples_per_class, num_features, num_classes, spread = spread)
+
+    # Select clustering algorithm
+    if clustering_algorithm == "kmeans"
+        model = Cluster.KMeans(k=num_classes, mode="kmeans")
+    elseif clustering_algorithm == "kmeanspp"
+        model = Cluster.KMeans(k=num_classes, mode="kmeanspp")
+    elseif clustering_algorithm == "bkmeans"
+        base_model = KMeans(k=2, mode="kmeans")
+        model = BKMeans(k=num_classes, kmeans=base_model)
+    end
+
+    # Fit the model
+    Cluster.fit!(model, data)
+
+    # Match the predicted labels to the true labels
+    matched_labels = match_labels(labels, model.labels, num_classes)
+
+    # Create subplots
+    create_subplots(data, labels, matched_labels, num_classes)
+end
 end
 
 # ╔═╡ Cell order:
@@ -113,5 +154,5 @@ end
 # ╠═146996c3-ea17-49e2-930e-40e1e04d7e49
 # ╠═c060f1ea-0a86-4fa3-980a-57b08912e87e
 # ╠═853c6935-f73c-4076-98af-0896ca0cdae2
+# ╠═fb517c86-f37a-44da-a5c0-9f97094215a5
 # ╠═726f9420-db77-442a-9fa3-09a1dad985c4
-# ╠═3197f17d-61c9-430c-9025-8e1de79e8f57
