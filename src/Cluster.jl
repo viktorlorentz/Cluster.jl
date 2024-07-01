4ad704422f6c6572178a100f9a0181e2b79b05f7
flohmacode <flohma01@gmail.com>
2024-07-01 17:11:25 +0200

    1701_01072024

--- src/Cluster.jl
+++ src/Cluster.jl
@@ -94,11 +94,18 @@ function init_centroids(X, K, mode)
     if !isa(K, Int) || K <= 0
         throw(ArgumentError("K must be a positive integer"))
     end
-    if mode != "kmeans" && mode != "kmeanspp"
+    if mode != "kmeans" && mode != "kmeanspp" && mode != "dc"
         throw(ArgumentError("mode must be either 'kmeans' or 'kmeanspp'"))
     end
 
     if mode == "kmeans"
+        # Generate random vector with the length of the data, then choose first K values
+        row, col = size(X)
+        permutation = randperm(row)
+        idx = permutation[1:K]
+        centroids = X[idx, :]
+    
+    elseif mode =="dc"
         # Generate random vector with the length of the data, then choose first K values
         row, col = size(X)
         permutation = randperm(row)
@@ -372,21 +379,41 @@ end
 
 
 
+mutable struct DC
+    k::Int
+    mode
+    max_try::Int
+    tol::Float64
+    centroids::Array{Float64,2}
+    labels::Array{Int,1}
+end
 
+"""
+    compute_objective_function(data, centroids,k) -> Array
 
+Computes the distance from each data point to each centroid.
 
+# Arguments
+- `data`: The input data matrix where each row is a data point.
+- `centroids`: The current centroids.
+- `k` the exponent of norm 
+# Returns
+A distance matrix `D` of size (number of data points, number of centroids), where `D[i, j]` is the distance from data point `i` to centroid `j`.
 
-
-
-
-
-
-
-
-
-
-
-
+# Examples
+```julia-repl
+X = [
+    1.0 1.0;
+    1.5 2.0;
+    3.0 4.0
+]
+centroids = [
+    1.0 1.0;
+    3.0 4.0
+]
+D = compute_distance(X, centroids)
+```
+"""
 function compute_objective_function(X, centroids,k)
     x = size(X)
     y = size(centroids)
@@ -397,7 +424,6 @@ function compute_objective_function(X, centroids,k)
 
     for i in 1:y[1]
         for j in 1:x[1]
-            #D[j, i] = log(norm(X[j,:].-centroids[i,:].^k .+ delta))
             D[j, i] = sqrt(sum((X[j, :] .- centroids[i, :]) .^ k .+ delta))
         end
     end
@@ -406,7 +432,32 @@ function compute_objective_function(X, centroids,k)
 
 end
 
+"""
+    update_centroids_dc(data, labelvector, model) -> Array
 
+Calculates new centroids based on the given data and label vector.
+
+# Arguments
+- `data`: The input data matrix where each row is a data point.
+- `labelvector`: The current labels of the data points.
+- `model`: The KMeans model.
+
+# Returns
+An array of new centroids.
+
+# Examples
+```julia-repl
+X = [
+    1.0 1.0;
+    1.5 2.0;
+    3.0 4.0;
+    5.0 6.0
+]
+labels = [1, 1, 2, 2]
+model = KMeans(k=2)
+new_centroids = update_centroids(X, labels, model)
+```
+"""
 function update_centroids_dc(X, label_vector, model)
     k = 3
     δ = 0.0001
@@ -414,7 +465,6 @@ function update_centroids_dc(X, label_vector, model)
     
     for i in 1:model.k
         
-
         # Mask for selecting points belonging to the i-th cluster
         mask = label_vector .== i
         
@@ -453,7 +503,29 @@ function update_centroids_dc(X, label_vector, model)
 
 end
 
+"""
+    fit!(model::KMeans, X)
 
+Runs the KMeans algorithm for the given data and model.
+
+# Arguments
+- `model::KMeans`: The KMeans model to be trained.
+- `X`: The input data matrix where each row is a data point.
+
+# Examples
+```julia-repl
+model = KMeans(k=3, mode=:kmeans)
+X = [
+    1.0 1.0;
+    1.5 2.0;
+    3.0 4.0;
+    5.0 6.0;
+    8.0 9.0;
+    10.0 11.0
+]
+fit!(model, X)
+```
+"""
 function fit_dc!(model, X,k)
     
     if size(X, 1) == 0 || size(X, 2) == 0
@@ -482,71 +554,48 @@ function fit_dc!(model, X,k)
         end
         
         model.centroids = new_centroids
-        print(model.centroids)
         
     end
-    
-    print("trained_centroids",model.centroids)
 
 end
 
-function predict_dc(model,X)
+"""
+    predict(model,data) -> Array
+
+Returns the cluster labels for the given data points.
+
+# Arguments
+- `model::KMeans`: The trained KMeans model.
+- `X::Array{Float64,2}`: The input data matrix where each row is a data point.
+
+# Returns
+An array of cluster labels for each data point.
+
+# Examples
+```julia-repl
+data = [
+    # Cluster 1
+    1.0 1.0 1.5;
+    1.5 2.0 1.6;
+    1.3 1.8 1.4;
+    # Cluster 2
+    5.0 7.0 3.5;
+    5.5 7.5 3.5;
+    6.0 7.0 3.5;
+    # Cluster 3
+    8.0 1.0 6.5;
+    8.5 1.5 6.5;
+    8.3 1.2 7.5;
+]
+test_data = [1.1 1.1 1.2]
+model = KMeans(k=3)
+fit!(model, data)
+labels = predict(model, test_data)
+```
+"""
+function predict(model::DC,X)
     D = compute_objective_function(X, model.centroids,2)
     return assign_center(D)
 end
-
-mutable struct DC
-    k::Int
-    mode
-    max_try::Int
-    tol::Float64
-    centroids::Array{Float64,2}
-    labels::Array{Int,1}
-end
-
-
-data_1 = [
-    # Cluster 1
-    1.0 1.0;
-    1.5 2.0;
-    # 1.3 1.8;
-    # 1.2 1.2;
-    # 0.8 0.9;
-    # 1.0 1.1;
-    # 1.3 1.3;
-    # 1.2 1.3;
-    # 1.3 1.4;
-    # 1.5 1.5;
-    
-    # Cluster 2
-    5.0 7.0;
-    5.5 7.5;
-    # 6.0 7.0;
-    # 5.8 7.2;
-    # 6.2 7.5;
-    # 5.9 6.8;
-    # 5.6 7.1;
-    # 6.3 7.6;
-    # 5.8 6.7;
-    # 5.8 7.7;
-    
-    # Cluster 3
-    8.0 1.0;
-    8.5 1.5;
-    # 8.3 1.2;
-    # 8.7 1.8;
-    # 8.4 1.4;
-    # 8.1 1.1;
-    # 8.6 1.6;
-    # 8.4 1.3;
-    # 8.3 1.5;
-    # 8.6 1.8
-]
-
-K = DC(3, "kmeans",20,1e-4,zeros(Float64, 0, 0), Int[])
-
-fit_dc!(K,data_1,2)
-
-print(predict_dc(K,[1.5 2.0]))
 
 end
