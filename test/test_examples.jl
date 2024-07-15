0b56c7c82a4ae4d6b1ce3265cae6dc1c84e33c1c
Viktor Lorentz <lorentz@campus.tu-berlin.de>
2024-07-15 23:30:37 +0200

    check if simple example creates plot

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,6 +1,13 @@
 @testset "Example Notebook Tests" begin
     include("../examples/notebook.jl")
+
 end
 @testset "Simple Example Tests" begin
     include("../examples/simple.jl")
+
+    # check if simple-cluster.png is created
+    @test isfile("simple-cluster.png")
+
+    # delete the file
+    rm("simple-cluster.png", force=true)
 end
